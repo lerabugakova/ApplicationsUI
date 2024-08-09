d27c2fe8617b852d1a56253f9037a397e62558a6
Valeriya <valeriya_bugakova@epam.com>
2024-08-09 13:12:12 +0500

    main table

---
+++ scripts/manager.js
@@ -0,0 +1,14 @@
+$(document).ready(function() 
+{
+    var role = localStorage.getItem('role');
+    var userName = localStorage.getItem('userName');
+
+    //if (role === 'Admin') 
+    //{
+    //    $('#addApplication').show();
+    //} 
+    //else 
+    //{
+    //    $('#addApplication').hide();
+    //}
+})
